9ea89229eccb13e1fbc4bc134a20f935af3396e2
Ned Richards <edwardlrichards@gmail.com>
2021-06-05 11:51:20 -0700

    focus on eigen-values as a way to save modes

--- pyducts/modes.py
+++ pyducts/modes.py
@@ -1,74 +1,94 @@
 import numpy as np
 from math import pi, sqrt
-import matplotlib.pyplot as plt
 from scipy.linalg import eigh_tridiagonal
 from scipy.optimize import brentq, newton
 from scipy.integrate import solve_ivp
 import numba as nb
 
-plt.ion()
 
-D = 100.
-fc = 400.
+# einsum implimentation of diagonal multiply
+n = 1000
+d0 = np.random.randn(n)
+d1 = np.random.randn(n-1)
+x = np.random.randn(n)
 
-ct = 0.
-rhot = 0.
+A_full = np.diag(d0) + np.diag(d1, k=1) + np.diag(d1, k=-1)
+A_sparse = np.zeros((3, n))
+test = np.zeros((3, n+2))
 
-c = 1500.
-rho = 1000.
+A_sparse[0, 1:] = d1
+A_sparse[1, ] = d0
+A_sparse[2, :-1] = d1
 
-cb = 1800.
-rhob = 0.
+ref = A_full @ x
+test[:, 1:-1] = A_sparse * x
+ref_sparse = test[2, :-2] + test[1, 1:-1] + test[0, 2:]
+assert(np.allclose(ref, ref_sparse))
+1/0
 
+def kr_modes(omega, k_min, k_max, z_max, c_ier, max_pow2=2):
+    """Compute eigenvalues between k_min and k_max"""
+    # bisection search for crossing regions
+    # determine max number of crossings
+    det, num_cross = det_estimate(omega, k_min, k_max, z_max, c_ier, max_pow2)
 
-# compute finite difference estimate of the modal equations
-k_water = 2 * pi * fc / c
-k_bottom = 2 * pi * fc / cb
+    # make array to save bisection results
+    regions = np.full((3, 2, num_cross + 1), np.nan)
+    regions[0, :, -1] = k_min
+    regions[1, :, -1] = det
+    regions[2, :, -1] = num_cross
 
-@nb.jit(nopython=True)
-def sturm_sequence(kr, dz, d0, y0):
-    """Scaled sturm sequence designed for mode finding"""
-    lam = dz ** 2 * (k_water ** 2 - kr ** 2)
-    y0[0] = 0
-    y0[1] = 1
-    for i in range(2, d0.size):
-        y0[i] = (2 - lam) * y0[i - 1] - y0[i - 2]
+    # evaluate determinant at start of region
+    det, num_cross = det_estimate(omega, k_max, k_max, z_max, c_ier, max_pow2)
+    regions[0, :, 0] = k_max
+    regions[1, :, 0] = det
+    regions[2, :, 0] = num_cross
 
-        # scale sequence
-        w = max(abs(y0[i]), abs(y0[i-1]))
-        if w > 1e10:
-            scale = 1e10 / w
-            y0[i] *= scale
-            y0[i - 1] *= scale
-        elif w < 1e-10:
-            scale = 1e-10 / w
-            y0[i] *= scale
-            y0[i - 1] *= scale
-    return y0 * dz
+    # bisect and split loop
+    _add_bisection(regions, omega, k_max, z_max, c_ier, max_pow2)
+    sr = _split_regions(regions)
+    while sr is not None:
+        _add_bisection(sr, omega, k_max, z_max, c_ier, max_pow2)
+        sr = _split_regions(regions)
 
-def det_estimate(kr):
-    """estimate the determinant with finite resolution sturm sequence"""
+    # compute eigen-values with bisection algorithm
+    kr_eig = []
+    rooter = lambda kr: det_estimate(omega, kr, k_max, z_max, c_ier, max_pow2)[0]
+    for i in range(regions.shape[-1] - 1):
+        kr_eig.append(brentq(rooter, regions[0, 1, i], regions[0, 0, i + 1]))
+
+    kr_eig = np.array(kr_eig)
+    return kr_eig
+
+def det_estimate(omega, kr, k_max, z_max, c_ier, max_pow2):
+    """combine sturm sequence and richardson extrapolation"""
     # divide integration axis in integer factors of fundimental length
-    kz = sqrt(k_water ** 2 - kr ** 2)
-    if kz > 0:
-        lz = 2 * pi / kz
-        dz_fundimental = D / max(np.ceil(D / lz), 1)
+
+    kz_ref = sqrt(k_max ** 2 - kr ** 2)
+    if kz_ref > 0:
+        lz = 2 * pi / kz_ref
+        dz_fundimental = z_max / max(np.ceil(z_max / lz), 1)
     else:
-        dz_fundimental = D
+        dz_fundimental = z_max
+
+    # number of iterations is set by finest sampling
+    pow2s = np.arange(max_pow2 + 1)
+    dzs = dz_fundimental / (8 * 2 ** pow2s)
+
+    num_steps = np.array(np.round(z_max / dzs), dtype=np.int, ndmin=1)
+    k_interval = np.array(num_steps[-1] / num_steps, dtype=np.int)
+    s_interval = np.array(num_steps / num_steps[0], dtype=np.int)
+
+    zaxis = np.arange(num_steps[-1] + 1) * dzs[-1]
+    c_up = c_ier(zaxis)
+    kz2 = (omega / c_up) ** 2 - kr ** 2
 
     det = []
     zs = []
-    for i, exp in enumerate(range(3, 5)):
-        dz = dz_fundimental / 2 ** exp
+    for k_step, s_step, dz in zip(k_interval, s_interval, dzs):
         # pressure release bottom formulation
-        k_ = np.full(int(np.round(D / dz)) + 1, k_water)
-        zaxis = np.arange(np.round(D / dz) + 1) * dz
-        d0 = -2 + (dz * k_) ** 2
-        y0 = np.empty(d0.size)
-        sturm = sturm_sequence(kr, dz, d0, y0)
-        det.append(sturm)
-    return det
-        det.append(sturm[2 ** i:][::2 ** i])
+        sturm = _sturm_sequence(kz2[::k_step], dz)
+        det.append(sturm[s_step:][::s_step])
     det = np.array(det)
 
     for i in range(det.shape[0] - 1):
@@ -79,46 +99,35 @@ def det_estimate(kr):
     # assume floating point will take care of 0 edge case
     num_cross = np.sum(np.abs(np.diff(sgn)) > 1)
 
-    return det[0, -1], num_cross
+    return det[-1, -1], num_cross
 
-def kr_modes(k_min, k_max):
-    """Compute eigenvalues between k_min and k_max"""
-    # bisection search for crossing regions
-    # determine max number of crossings
-    det, num_cross = det_estimate(k_bottom)
+@nb.jit(nopython=True)
+def _sturm_sequence(kz2, dz):
+    """Scaled sturm sequence designed for mode finding"""
+    y_out = np.empty_like(kz2)
+    lam = dz ** 2 * kz2
+    y_out[0] = 0
+    y_out[1] = 1
 
-    # make array to save bisection results
-    regions = np.full((3, 2, num_cross + 1), np.nan)
-    regions[0, :, -1] = k_bottom
-    regions[1, :, -1] = det
-    regions[2, :, -1] = num_cross
+    for i in range(2, y_out.size):
+        y_out[i] = (2 - lam[i]) * y_out[i - 1] - y_out[i - 2]
+        # scale sequence
+        w = max(abs(y_out[i]), abs(y_out[i-1]))
+        if w > 1e10:
+            scale = 1e10 / w
+            y_out[i] *= scale
+            y_out[i - 1] *= scale
+        elif w < 1e-10:
+            scale = 1e-10 / w
+            y_out[i] *= scale
+            y_out[i - 1] *= scale
+    return y_out * dz
 
-    # evaluate determinant at start of region
-    det, num_cross = det_estimate(k_water)
-    regions[0, :, 0] = k_water
-    regions[1, :, 0] = det
-    regions[2, :, 0] = num_cross
 
-    # bisect and split loop
-    _add_bisection(regions)
-    sr = _split_regions(regions)
-    while sr is not None:
-        _add_bisection(sr)
-        sr = _split_regions(regions)
-
-    # compute eigen-values with bisection algorithm
-    kr_eig = []
-    rooter = lambda kr: det_estimate(kr)[0]
-    for i in range(regions.shape[-1] - 1):
-        kr_eig.append(brentq(rooter, regions[0, 1, i], regions[0, 0, i + 1]))
-
-    kr_eig = np.array(kr_eig)
-    return kr_eig
-
-def _add_bisection(sr):
+def _add_bisection(sr, omega, k_max, z_max, c_ier, max_pow2):
     # first bisection
     k_test = (sr[0, 1, 0] + sr[0, 0, -1]) / 2
-    det, num_cross = det_estimate(k_test)
+    det, num_cross = det_estimate(omega, k_test, k_max, z_max, c_ier, max_pow2)
     cross_index = int(num_cross - sr[2, 0, 0])
     if np.isnan(sr[0, 0, cross_index]):
         sr[0, :, cross_index] = k_test
@@ -134,7 +143,7 @@ def _add_bisection(sr):
 
 def _split_regions(sr):
     # are there any regions yet undefined?
-    isunset = np.isnan(regions[0, 0, :])
+    isunset = np.isnan(sr[0, 0, :])
     if isunset.sum() == 0:
         # break condition
         return
@@ -142,7 +151,5 @@ def _split_regions(sr):
     # focus on first region
     roi_start = np.argmax(isunset)
     roi_end = np.argmax(np.bitwise_not(isunset[roi_start: ]))
-    sub_region = regions[:, :, roi_start - 1: roi_start + roi_end + 1]
+    sub_region = sr[:, :, roi_start - 1: roi_start + roi_end + 1]
     return sub_region
-
-

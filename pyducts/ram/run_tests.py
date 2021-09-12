9a8d13276c5686f081f7265bbf6a2bc992a258d3
Ned Richards <edwardlrichards@gmail.com>
2021-09-12 13:11:21 -0700

    compiled sources for tests

--- pyducts/ram/run_tests.py
+++ pyducts/ram/run_tests.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 from ram_wrapper import read_line, read_grid
 
 ram_exe = './bin/ram'
-ram_typed_exe = './bin/ram_typed'
+ram_typed_exe = './bin/ram_v1'
 
 # typing changes tl by a bit, put absolute threshold on accuracy
 eps = 0.1
@@ -20,10 +20,9 @@ rg, zg, tlg = read_grid(is_standard=True)
 # compare with typed ram output
 rout = check_output(ram_typed_exe)
 r_t, p_t = read_line(is_standard=False)
-tl_t = 20 * np.log10(np.abs(p_t))
+tl_t = -20 * np.log10(np.abs(p_t) + np.spacing(1))
 rg_t, zg_t, p_t = read_grid(is_standard=False)
-tlg_t = 20 * np.log10(np.abs(p_t))
-
+tlg_t = -20 * np.log10(np.abs(p_t) + np.spacing(1))
 
 # confirm that all error occurs at very low tl
 if np.any(np.abs(tl-tl_t) > eps):
@@ -31,7 +30,6 @@ if np.any(np.abs(tl-tl_t) > eps):
 if np.any(np.abs(tlg-tlg_t) > eps):
     assert(np.min(tlg_t[np.abs(tlg-tlg_t) > eps]) > 80)
 
-1/0
 # run spice example
 shutil.copy('./tests/spice.in', 'ram.in')
 rout = check_output(ram_exe)
@@ -42,13 +40,13 @@ rg, zg, tlg = read_grid(is_standard=True)
 
 # compare with typed ram output
 rout = check_output(ram_typed_exe)
-r_t, tl_t = read_line(is_standard=False)
-rg_t, zg_t, tlg_t = read_grid(is_standard=False)
+r_t, p_t = read_line(is_standard=False)
+tl_t = -20 * np.log10(np.abs(p_t) + np.spacing(1))
+rg_t, zg_t, p_t = read_grid(is_standard=False)
+tlg_t = -20 * np.log10(np.abs(p_t) + np.spacing(1))
 
 # confirm that all error occurs at very low tl
 if np.any(np.abs(tl-tl_t) > eps):
     assert(np.min(tl_t[np.abs(tl-tl_t) > eps]) > 80)
 if np.any(np.abs(tlg-tlg_t) > eps):
     assert(np.min(tlg_t[np.abs(tlg-tlg_t) > eps]) > 80)
-
-
